015a922aa711fcf7d84d314cddad0484018189b9
Luan P. Coelho <luan@byteslivres.com.br>
2020-05-19 18:40:54 -0300

    Add exception for not exist file

---
+++ app/Exceptions/NotExistFile.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+class NotExistFile extends \UnexpectedValueException
+{
+
+    private $body;
+
+    public function __construct($body)
+    {
+        $this->body = $body;
+    }
+}
