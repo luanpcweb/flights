3cde06d52973a445d055774f531957be4b7a05ee
Luan P. Coelho <luan@byteslivres.com.br>
2020-06-16 18:40:39 -0300

    Api ticket search

--- app/Http/Controllers/FlightsController.php
+++ app/Http/Controllers/FlightsController.php
@@ -9,12 +9,20 @@ class FlightsController extends Controller
     public function search(Request $request)
     {
 
-        $from = $request->from;
-        $to = $request->to;
-        $departure_date = $request->departure_date;
-        $return_date = $request->return_date;
-        $max_price = $request->max_price;
+        $tam = new \App\Repository\TAM(file_get_contents(__DIR__ . '/../../TAM.json'));
+        $tap = new \App\Repository\TAP(file_get_contents(__DIR__ . '/../../TAP.xml'));
 
+        $search = new \App\Service\FlightSearcher($tap, $tam);
+
+        $tickets = $search->search(
+            $request->get('from'),
+            $request->get('to'),
+            new \DateTime($request->get('departure_date')),
+            $request->get('price'),
+            $request->get('return_date') ? new \DateTime($request->get('return_date')) : null
+        );
+
+        return $tickets;
 
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('search', 'FlightsController@search');
+Route::get('/search', 'FlightsController@search');
